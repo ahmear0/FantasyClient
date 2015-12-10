612f0837c1e89d95029d83369b2a74e25e2c909d
Asheq Ahmed <aahmed@glneurotech.com>
2015-12-09 23:13:58 -0500

    included queries from ppt

--- exampleQueries.sql
+++ exampleQueries.sql
@@ -1,9 +1,76 @@
+/* List the week 1 leader in Passing Yards */
+SELECT lastName, firstName, passYds
+From Player Natural Join playerScores 
+WHERE passYds IS NOT NULL
+AND week=1
+ORDER BY passYds DESC
+LIMIT 1;
 
+/* List the winning fantasy teams from week 1 */
+SELECT userName1 as winner, score1 as winScore
+FROM Matchup
+WHERE week=1 AND score1>score2 
+UNION 
+(SELECT userName2, score2
+FROM Matchup
+WHERE week=1 AND score2>score1)
+
+
+/* Which professional teams do Ron's players belong to?
+How many belong to each professional team? */
+SELECT teamName, COUNT(teamName)
+FROM (FantasyTeam NATURAL JOIN Rostered NATURAL JOIN Player)
+WHERE userName='Ron'
+GROUP BY teamName
+ORDER BY count(teamName) DESC;
+
+
+/* Find Ron's Opponent's Roster for Week 1 */
+SELECT playerID, position, lastName, firstName
+FROM (Player P NATURAL JOIN Rostered R), FantasyTeam Opponent, Matchup M
+WHERE M.userName1 = 'Ron' AND
+  	M.userName2 = Opponent.userName AND
+  	Opponent.userName = R.userName AND
+  	M.week = 1
+UNION
+(SELECT playerID, position, lastName, firstName
+FROM (Player P NATURAL JOIN Rostered R), FantasyTeam Opponent, Matchup M
+WHERE M.userName2 = 'Ron' AND
+  	M.userName1 = Opponent.userName AND
+  	Opponent.userName = R.userName AND
+  	M.week = 1);
+
+
+/* Find Ron's Opponent's Starting Roster for Week 1 */
+SELECT playerID, position, lastName, firstName
+FROM (Player P NATURAL JOIN Rostered R), FantasyTeam Opponent, Matchup M
+WHERE M.userName1 = 'Ron' AND
+  	M.userName2 = Opponent.userName AND
+  	Opponent.userName = R.userName AND
+  	M.week = 1 AND start=1
+UNION
+(SELECT playerID, position, lastName, firstName
+FROM (Player P NATURAL JOIN Rostered R), FantasyTeam Opponent, Matchup M
+WHERE M.userName2 = 'Ron' AND
+  	M.userName1 = Opponent.userName AND
+  	Opponent.userName = R.userName AND
+  	M.week = 1 AND start=1);
+
+
+/* List players who accumulated some (non-zero) yardage in week 1 */
+select firstName, lastName, passYds, rushYds, recYds, retYds, passYds + rushYds + recYds + retYds as totalYds from playerScores Natural Join Player where week=11 AND passYds IS NOT NULL
+    -> UNION
+    -> (select firstName, lastName, passYds, rushYds, recYds, retYds, passYds + rushYds + recYds + retYds as totalYds from playerScores Natural Join Player where week=11 AND rushYds IS NOT NULL)
+    -> UNION
+    -> (select firstName, lastName, passYds, rushYds, recYds, retYds, passYds + rushYds + recYds + retYds as totalYds from playerScores Natural Join Player where week=11 AND recYds IS NOT NULL)
+    -> UNION
+    -> (select firstName, lastName, passYds, rushYds, recYds, retYds, passYds + rushYds + recYds + retYds as totalYds from playerScores Natural Join Player where week=11 AND retYds IS NOT NULL);
 
 /* List all wide recievers that play for the New England Patriots */
 SELECT playerID, firstName, lastName 
 FROM Player
 WHERE position='WR' AND teamName='NWE';
+
 
 /* List the top-10 players with the most rushing yards */
 SELECT lastName, firstName, teamName, sum(rushYds) 
@@ -12,6 +79,4 @@ WHERE position='RB'
 GROUP BY playerID 
 ORDER BY sum(rushYds) 
 DESC limit 10;
-
-
 
