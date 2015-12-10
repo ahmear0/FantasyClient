0e62e9c1e505dba10a32c8f388473691a7021e88
Asheq Ahmed <aahmed@glneurotech.com>
2015-12-09 23:07:35 -0500

    updated queries file--copy/paste into sql

--- exampleQueries.sql
+++ exampleQueries.sql
@@ -1,3 +1,17 @@
+
+
+/* List all wide recievers that play for the New England Patriots */
 SELECT playerID, firstName, lastName 
 FROM Player
 WHERE position='WR' AND teamName='NWE';
+
+/* List the top-10 players with the most rushing yards */
+SELECT lastName, firstName, teamName, sum(rushYds) 
+FROM playerScores Natural Join Player 
+WHERE position='RB' 
+GROUP BY playerID 
+ORDER BY sum(rushYds) 
+DESC limit 10;
+
+
+
