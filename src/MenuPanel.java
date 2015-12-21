1d02adca6bfaf797ee53546a13b62b91a78817b0
Asheq Ahmed <aahmed@glneurotech.com>
2015-12-21 13:37:10 -0500

    clean up button formatting

--- src/MenuPanel.java
+++ src/MenuPanel.java
@@ -5,6 +5,7 @@ import javax.swing.JScrollPane;
 import javax.swing.BorderFactory;
 import java.awt.Color;
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import javax.swing.table.DefaultTableModel;
@@ -16,26 +17,16 @@ import javax.swing.border.Border;
 public class MenuPanel extends JPanel {
 
     private String clientUser;
-    //private JButton refreshButton, test2;
 
     public MenuPanel(String _clientUser) {
 
+
         setBackground(new Color(220,240,240));
         setSize(new Dimension(400,70));
+        setLayout(new FlowLayout());
 
         clientUser = _clientUser;
-        // refreshButton = makeMenuButton("Refresh");
-        // test2 = makeMenuButton("Test1");
-
-        // //add buttons to the frame
-        // add(refreshButton);
-        // add(test2);
     }
-
-    // public Dimension getPreferredSize() {
-    //     return new Dimension(250,200);
-    // }
-
 
     private static JButton makeMenuButton(String text) {
       JButton button = new JButton(text);
