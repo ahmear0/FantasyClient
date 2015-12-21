fa869a89a1d4fa029c015c60ad6b4d4d9618e35e
Asheq Ahmed <aahmed@glneurotech.com>
2015-12-21 13:39:32 -0500

    add standings tab and fix buttons

--- src/FantasyClient.java
+++ src/FantasyClient.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseAdapter;
 import java.awt.Color;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -26,25 +27,74 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 import javax.swing.border.Border;
 import javax.swing.JOptionPane;
+import java.lang.NumberFormatException;
 
-public class FantasyClient extends JFrame implements ActionListener {
+public class FantasyClient extends JFrame {
 
     public final static String clientUser = "Asheq";
     private int currentWeek = 14;
     private boolean isAdmin = true;
-    private static Object [][] playerData;
+    private static Object [][] playerData, standingsData;
     private static JButton refreshButton, scrapeButton, refresh;
     private static databaseAPI database;
     private static RosterPanel rosterTab;
     private static MenuPanel menuPanel;
     private static JPanel matchupTab, playerTab, standingsTab;
+    private JFrame currentFrame;
 
     private FantasyClient(String title) {
 
         super(title);
+        currentFrame = this;
         this.getContentPane().setLayout(new BorderLayout());
         refreshButton = makeMenuButton("Refresh");
+        refreshButton.addMouseListener(new MouseAdapter() { 
+            public void mouseEntered(MouseEvent me)
+            {
+                refreshButton.setBackground(new Color(0,200,200));
+            }
+            public void mouseExited(MouseEvent me)
+            {
+                refreshButton.setBackground(Color.WHITE);
+            }
+            public void mousePressed(MouseEvent me) 
+            { 
+                refreshButton.setBackground(new Color(0,180,180));
+            } 
+            public void mouseClicked(MouseEvent me)
+            {
+                refreshButton.setBackground(Color.WHITE);
+                rosterTab.refresh();
+                validate();
+            }
+        }); 
+
+
         scrapeButton = makeMenuButton("Scrape");
+        scrapeButton.addMouseListener(new MouseAdapter() { 
+          public void mouseClicked(MouseEvent me) 
+          { 
+            scrapeButton.setBackground(Color.YELLOW);
+            int week = 0;
+            try
+            {
+                week = Integer.parseInt(JOptionPane.showInputDialog("Enter Week:"));
+                if (week>=1 && week<=17)
+                {
+                    setVisible(false);
+                    scrapeAPI.scrapeTheWeek(week);
+                    setVisible(true);
+                }
+                else
+                {
+                    throw new NumberFormatException();
+                }
+            } catch (NumberFormatException exception)
+            {
+                JOptionPane.showMessageDialog(currentFrame, "Invalid Week.  Please use a valid integer, 1-17.", "Invalid Input", JOptionPane.WARNING_MESSAGE);
+            }
+          } 
+        }); 
     }
 
     //protected void initUI() {
@@ -57,6 +107,12 @@ public class FantasyClient extends JFrame implements ActionListener {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         menuPanel = new MenuPanel(clientUser);
+        
+
+        FlowLayout menuLayout = new FlowLayout();
+        menuLayout.setAlignment(FlowLayout.LEFT);
+        menuPanel.setLayout(menuLayout);
+
         menuPanel.add(refreshButton);
         menuPanel.add(scrapeButton);
 
@@ -78,6 +134,18 @@ public class FantasyClient extends JFrame implements ActionListener {
         JScrollPane playerContainer = new JScrollPane(playerTable);
         playerTab.add(playerContainer, BorderLayout.CENTER);
 
+
+        //standingsTab
+        String [] standingsColumns = new String[3];
+        standingsColumns[0] = "Fantasy Team";
+        standingsColumns[1] = "Win";
+        standingsColumns[2] = "Loss";
+
+        standingsData = database.getStandings();
+        JTable standingsTable = new JTable(new DefaultTableModel(standingsData,standingsColumns));
+        JScrollPane standingsContainer = new JScrollPane(standingsTable);
+        standingsTab.add(standingsContainer, BorderLayout.CENTER);
+
         tabPanel.addTab("Roster",rosterTab);
         tabPanel.addTab("Matchup",matchupTab);
         tabPanel.addTab("Players",playerTab);
@@ -90,14 +158,14 @@ public class FantasyClient extends JFrame implements ActionListener {
         frame.getContentPane().add(menuPanel, BorderLayout.NORTH);
         frame.getContentPane().add(tabPanel, BorderLayout.CENTER);
         frame.pack(); 
-        frame.setBackground(Color.white);
+        frame.setBackground(Color.WHITE);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
 
     private JButton makeMenuButton(String text) {
         JButton button = new JButton(text);
-        button.addActionListener(this);
+        button.setOpaque(true);
         button.setForeground(Color.BLACK);
         button.setBackground(Color.WHITE);
         Border line = new LineBorder(Color.BLACK);
@@ -106,45 +174,4 @@ public class FantasyClient extends JFrame implements ActionListener {
         button.setBorder(compound);
         return button;
     }
-
-
-    @Override
-    public void actionPerformed(ActionEvent ae) 
-    {
-        String action = ae.getActionCommand();
-        if (action.equals("Refresh")) 
-        {
-        	refreshButton.setBackground((Color.BLUE));
-            rosterTab.refresh();
-            validate();
-        }
-        else if (action.equals("Scrape"))
-        {
-        	int week = Integer.parseInt(JOptionPane.showInputDialog("Enter Week:"));
-        	setVisible(false);
-        	scrapeAPI.scrapeTheWeek(week);
-        	setVisible(true);
-        }
-    }
-
-    public void mouseEntered(MouseEvent e) 
-    {
-        refreshButton.setBackground(Color.BLUE);
-    }
-    
-    public void mouseExited(MouseEvent e) 
-    {
-        refreshButton.setBackground(Color.WHITE);    
-    }   
-    public void mouseClicked(MouseEvent e) 
-    {
-
-    }
-    public void mousePressed(MouseEvent e) 
-    {
-        refreshButton.setBackground(Color.BLACK);
-    }  
-    public void mouseReleased(MouseEvent e)
-    {}
-
 }
